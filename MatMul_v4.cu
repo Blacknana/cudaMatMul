d06ac683f8c4822c1f44dcbaf0dc7aca896e81a1
sunqianqi <sunqianqi18@mails.ucas.ac.cn>
2021-09-26 10:56:40 +0800

    log: 9.26 10:56 unknown error(999)

--- MatMul_v4.cu
+++ MatMul_v4.cu
@@ -8,55 +8,62 @@ using namespace std;
 
 const int Row = 1024;
 const int Col = 1024;
-const int Blocksize = 32;
+const int Block_x = 32;
+const int Block_y = 16;
 
 __global__ void myMatMulOnGPU(float* M, float* N, float* P, int width)
 {
-    int blockRow = blockIdx.x;
-    int blockCol = blockIdx.y;
-    const int blockstride = Blocksize * 2;
+    int blockRowIdx = blockIdx.x;
+    int blockColIdx = blockIdx.y;
+    const int blockRowStride = Block_x * 2;
+    const int blockColStride = Block_y * 2;
+    const int banksize = Block_y * 2;
     int row = threadIdx.x;
     int col = threadIdx.y;
 
+    // loadstore-compute pipeline
+    int load_idx = 0;
+    int write_idx = 1;
+    __shared__ float Msub[2][banksize * blockRowStride];
+    __shared__ float Nsub[2][blockColStride * banksize];
+
     float sum[4] = {0, 0, 0, 0};
 
-    for (int i = 0; i < (width / blockstride); i++) {
-
-        __shared__ float Msub[blockstride * blockstride];
-        __shared__ float Nsub[blockstride * blockstride];
-
-        // each thread get 4 element(128bit) from M and N continuously
-        int M_loc = (i * blockstride + col * 2 + row * 4 / blockstride) * width + (blockRow * blockstride + row * 4 % blockstride);
-        int N_loc = (blockCol * blockstride + col * 2 + row * 4 / blockstride) * width + (i * blockstride + row * 4 % blockstride);
-        int MNsub_loc = (col * 2) * blockstride + row * 4;
-
+    for (int i = 0; i < (width / banksize); i++) {
+        // each thread get 4 element(128bit) from M continuously
+        int M_loc = (i * banksize + col * 2 + row * 4 / blockRowStride) * width + (blockRowIdx * blockRowStride + row * 4 % blockRowStride);
+        int Msub_loc = (col * 2) * blockRowStride + row * 4;
         for (int j = 0; j < 4; j++)
-            Msub[MNsub_loc + j] = M[M_loc + j];
-        for (int j = 0; j < 4; j++)
-            Nsub[MNsub_loc + j] = N[N_loc + j];
+            Msub[write_idx][Msub_loc + j] = M[M_loc + j];
+
+        // each thread get 2 element(64bit) from N continuously
+        int N_loc = (blockColIdx * blockColStride + col * 2 + row * 2 / banksize) * width + (i * banksize + row * 2 % banksize);
+        int Nsub_loc = (col * 2) * banksize + row * 2;
+        for (int j = 0; j < 2; j++)
+            Nsub[write_idx][Nsub_loc + j] = N[N_loc + j];
 
         // make sure that the sub-matrices are loaded
         __syncthreads();
 
+        // switch
+        load_idx ^= 1;
+        write_idx ^= 1;
+
         // each thread compute 4 element
-        // i.e. (x, y), (x + Blocksize, y), (x, y + Blocksize), (x + Blocksize, y + Blocksize)
-        for (int j = 0; j < blockstride; j++)
+        // i.e. (x, y), (x + Block_x, y), (x, y + Block_y), (x + Block_x, y + Block_y)
+        for (int j = 0; j < banksize; j++)
         {
-            sum[0] += Msub[j * blockstride + row] * Nsub[col * blockstride + j];
-            sum[1] += Msub[j * blockstride + row + Blocksize] * Nsub[col * blockstride + j];
-            sum[2] += Msub[j * blockstride + row] * Nsub[(col + Blocksize) * blockstride + j];
-            sum[3] += Msub[j * blockstride + row + Blocksize] * Nsub[(col + Blocksize) * blockstride + j];
+            sum[0] += Msub[write_idx][j * blockRowStride + row] * Nsub[load_idx][col * banksize + j];
+            sum[1] += Msub[write_idx][j * blockRowStride + row + Block_x] * Nsub[load_idx][col * banksize + j];
+            sum[2] += Msub[write_idx][j * blockRowStride + row] * Nsub[load_idx][(col + Block_y) * banksize + j];
+            sum[3] += Msub[write_idx][j * blockRowStride + row + Block_x] * Nsub[load_idx][(col + Block_y) * banksize + j];
         }
-        
-        // make sure that preceding computation is done
-        // before the next iteration
-        __syncthreads();
     }
 
-    P[(blockCol * blockstride + col) * width + (blockRow * blockstride + row)] = sum[0];
-    P[(blockCol * blockstride + col) * width + (blockRow * blockstride + row + Blocksize)] = sum[1];
-    P[(blockCol * blockstride + col + Blocksize) * width + (blockRow * blockstride + row)] = sum[2];
-    P[(blockCol * blockstride + col + Blocksize) * width + (blockRow * blockstride + row + Blocksize)] = sum[3];
+    P[(blockColIdx * blockColStride + col) * width + (blockRowIdx * blockRowStride + row)] = sum[0];
+    P[(blockColIdx * blockColStride + col) * width + (blockRowIdx * blockRowStride + row + Block_x)] = sum[1];
+    P[(blockColIdx * blockColStride + col + Block_y) * width + (blockRowIdx * blockRowStride + row)] = sum[2];
+    P[(blockColIdx * blockColStride + col + Block_y) * width + (blockRowIdx * blockRowStride + row + Block_x)] = sum[3];
 }
 
 int main()
@@ -85,7 +92,7 @@ int main()
     CHECK(cudaEventCreate(&stop));
 
     //init block and grid dim
-    dim3 threadPerBlock(Blocksize, Blocksize);
+    dim3 threadPerBlock(Block_x, Block_y);
     dim3 blockNumber((Col + threadPerBlock.x - 1) / (2 * threadPerBlock.x), (Row + threadPerBlock.y - 1) / (2 * threadPerBlock.y));
     printf("Block(%d,%d)   Grid(%d,%d).\n", threadPerBlock.x, threadPerBlock.y, blockNumber.x, blockNumber.y);
 
